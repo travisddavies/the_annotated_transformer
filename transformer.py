7c4ec2711786035386b371394f153499aa0d5fbc
travisddavies <travisddavies@gmail.com>
2024-05-24 21:04:05 +1000

    added encoder layer and layernorm

--- transformer.py
+++ transformer.py
@@ -49,6 +49,11 @@ class EncoderDecoder(nn.Module):
         return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
 
 
+def clones(module, N):
+    "Produce N identical layers"
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
 class Generator(nn.Module):
     "Define standard linear + softmax generation step"
 
@@ -58,3 +63,33 @@ class Generator(nn.Module):
 
     def forward(self, x):
         return log_softmax(self.proj(x), dim=-1)
+
+
+class LayerNorm(nn.Module):
+    "Construct a layer norm module"
+
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class Encoder(nn.Module):
+    "Core encoder is a stack of N layers"
+
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        "Pass the input (and mask) through each layer in turn"
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
