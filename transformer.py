a056ccc53cbbf86e22da9ad85633986931068b19
travisddavies <travisddavies@gmail.com>
2024-05-24 22:00:40 +1000

    added attention function

--- transformer.py
+++ transformer.py
@@ -168,3 +168,15 @@ def subsequent_mask(size):
     )
 
     return subsequent_mask == 0
+
+
+def attention(query, key, value, mask=None, dropout=None):
+    "Compute 'Scaled dot product Attention'"
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = scores.softmax(dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
