c0844d2e2b20598c4645762ac6f7823b585a8d36
travisddavies <travisddavies@gmail.com>
2024-05-25 00:05:50 +1000

    removed non-used modules

--- transformer.py
+++ transformer.py
@@ -1,25 +1,10 @@
-import os
-from os.path import exists
 import torch
 import torch.nn as nn
-from torch.nn.functional import log_softmax, pad
 import math
 import copy
-import time
-from torch.optim.lr_scheduler import LambdaLR
-import pandas as pd
-from torchtext.data.functional import to_map_style_dataset
-from torch.utils.data import DataLoader
-from torchtext.vocab import build_vocab_from_iterator
-import torchtext.datasets as datasets
-import spacy
 import warnings
-from torch.utils.data.distributed import DistributedSampler
-import torch.multiprocessing as mp
-from torch.nn.parallel import DistributedDataParallel as DDP
 
 warnings.filterwarnings('ignore')
-RUN_EXAMPLES = True
 
 
 class EncoderDecoder(nn.Module):
