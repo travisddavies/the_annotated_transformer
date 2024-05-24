1d622ced494b2b56736d8808cb684a01500ec35a
travisddavies <travisddavies@gmail.com>
2024-05-24 21:12:39 +1000

    added annotations explaining the layer norm

--- transformer.py
+++ transformer.py
@@ -70,11 +70,15 @@ class LayerNorm(nn.Module):
 
     def __init__(self, features, eps=1e-6):
         super(LayerNorm, self).__init__()
+        # These are learnable parameters
+        # a_2 acts as weights
         self.a_2 = nn.Parameter(torch.ones(features))
+        # b_2 acts as a bias
         self.b_2 = nn.Parameter(torch.zeros(features))
         self.eps = eps
 
     def forward(self, x):
+        # This is like a derivative of the normal distribution
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
         return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
